e2329543111cdf6decf3c4c8a4765f16e7edd880
bb2504 <68122285+bb2504@users.noreply.github.com>
2022-03-11 23:25:19 +0100

    Update config.py
    Update config file

--- src/getcontact/config.py
+++ src/getcontact/config.py
@@ -24,14 +24,14 @@ class Config:
     AES_KEY = "dd074ed6e8c64bc65cabdfdca052f16b187e5cbbc501e22d98dae2f9899fe543"
 
     # May need to be changed
-    APP_VERSION = "4.9.1"
+    APP_VERSION = "5.6.2"
     API_VERSION = "v2.8"
     ANDROID_OS = "android 6.0"
     DEVICE_ID = "8edbe110a4079830"
 
     # Default values
     COUNTRY = "RU"
-    HMAC_KEY = "2Wq7)qkX~cp7)H|n_tc&o+:G_USN3/-uIi~>M+c ;Oq]E{t9)RC_5|lhAA_Qq%_4"
+    HMAC_KEY = "y1gY|J%&6V kTi$>_Ali8]/xCqmMMP1$*)I8FwJ,*r_YUM 4h?@7+@#<>+w-e3VW"
     MOD_EXP = 900719925481
 
     VERBOSE = False
